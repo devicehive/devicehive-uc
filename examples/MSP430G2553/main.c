f3bc360b87fe810ef095262b0603f4a8374a5a0a
Sergey Polichnoy <sergey.polichnoy@dataart.com>
2013-01-28 14:56:54 +0400

    send notification at startup
    cleanup unused code and defines
    do not send failed results in case of serial communication error

--- examples/MSP430G2553/main.c
+++ examples/MSP430G2553/main.c
@@ -7,14 +7,12 @@
 #define GLED_PIN    BIT6 ///< @brief The green LED pin number.
 #define BTN_PIN     BIT3 ///< @brief The push button pin number.
 
+#define DEVICE_ID               "c73ccf23-8bf5-4c2c-b330-ead36f469d1a"
 #define DEVICE_KEY              "uC_test"
 #define DEVICE_NAME             "MSP430 Device"
 #define DEVICE_CLASS_NAME       "SimpleDevice"
 #define DEVICE_CLASS_VERSION    "1.1"
 
-// {C73CCF23-8BF5-4C2C-B330-EAD36F469D1A}
-//const GUID DeviceID =           { 0xc73ccf23, 0x8bf5, 0x4c2c, { 0xb3, 0x30, 0xea, 0xd3, 0x6f, 0x46, 0x9d, 0x1a } };
-#define DEVICE_ID               "c73ccf23-8bf5-4c2c-b330-ead36f469d1a"
 
 #define NP_EQUIPMENT            "equipment"
 #define NP_STATE                "state"
@@ -33,41 +31,12 @@
 #define BTN_EQP_CODE            "BTN"
 #define BTN_EQP_TYPE            "Button"
 
-#define ERR_UNKNOWN_CMD         "Unknown command"
-#define ERR_FAIL                "Failed"
-#define ERR_INVALID_VALUE       "Invalid data. 1-byte Boolean value expected."
-#define ERR_EMPTY_EQUIPMENT     "Failed to receive the equipment name."
-#define ERR_INVALID_EQUIPMENT   "The specified equipment not supported."
-#define ERR_TIMEOUT             "Failed to retrieve LED state due to timeout."
-
+#define ST_SUCCESS              "Success"
+#define ST_FAIL                 "Failed"
 #define RESULT_OK               "OK"
-#define ST_SUCCESS              "Led command successfully executed"
 
-#define D2G_EQUIPMENT           (MIN_CUSTOM_INTENT)
-#define G2D_LED                 (MIN_CUSTOM_INTENT + 1)
-
-//static /*const*/ Equipment EQUIPMENTS[] =
-//{
-//    EQUIPMENT_INIT(LED0_EQP_NAME, LED0_EQP_CODE, LED0_EQP_TYPE),
-//    EQUIPMENT_INIT(LED1_EQP_NAME, LED1_EQP_CODE, LED1_EQP_TYPE),
-//    EQUIPMENT_INIT(BTN_EQP_NAME, BTN_EQP_CODE, BTN_EQP_TYPE)
-//};
-//
-//static /*const*/ Parameter NOTIFICATION_PARAMS[] =
-//{
-//    PARAMETER_INIT(DT_STRING, NP_EQUIPMENT),
-//    PARAMETER_INIT(DT_BOOLEAN, NP_STATE)
-//};
-//
-//static /*const*/ Notification NOTIFICATIONS[] =
-//{
-//    NOTIFICATION_INIT(D2G_EQUIPMENT, NOTIFY_EQUIPMENT, NOTIFICATION_PARAMS)
-//};
-//
-//static /*const*/ Command COMMANDS[] =
-//{
-//    COMMAND_INIT(G2D_LED, LED_CMD_NAME, NOTIFICATION_PARAMS)
-//};
+#define D2G_EQUIPMENT           (256)
+#define G2D_LED                 (257)
 
 
 static int strcmp(const char *s1, const char *s2)
@@ -85,23 +54,6 @@ static int strcmp(const char *s1, const char *s2)
 /** @brief Send device registration metadata. */
 static void SendRegMessage()
 {
-//    RegData rd;
-//
-//    rd.DeviceID = DeviceID;
-//    rd.DeviceKey = DEVICE_KEY;
-//    rd.DeviceName = DEVICE_NAME;
-//    rd.DeviceClassName = DEVICE_CLASS_NAME;
-//    rd.DeviceClassVersion = DEVICE_CLASS_VERSION;
-//    rd.Equipment.Length = CountOf(EQUIPMENTS);
-//    rd.Equipment.Items = EQUIPMENTS;
-//    rd.Notifications.Length = CountOf(NOTIFICATIONS);
-//    rd.Notifications.Items = NOTIFICATIONS;
-//    rd.Commands.Length = CountOf(COMMANDS);
-//    rd.Commands.Items = COMMANDS;
-//
-//    SendRegistrationData(&rd);
-
-
     SendRegistration2Data("{"
         "id:\"" DEVICE_ID "\","
         "key:\"" DEVICE_KEY "\","
@@ -168,16 +120,12 @@ static void ProcessCommand(const MessageHeader *msgh)
                         SendNotificationData(cmdId, ST_SUCCESS, RESULT_OK);
                         SendEquipmentNotification(LED1_EQP_CODE, state);
                     }
+                    else
+                        SendNotificationData(cmdId, ST_FAIL, "The specified equipment not supported");
                 }
             }
-            else
-                SendNotificationData(cmdId, ERR_INVALID_VALUE, ERR_FAIL);
         }
-        else
-            SendNotificationData(cmdId, ERR_EMPTY_EQUIPMENT, ERR_FAIL);
     }
-    else
-        SendNotificationData(0, ERR_TIMEOUT, ERR_FAIL);
 }
 
 /** @brief The button state cache. */
@@ -209,6 +157,12 @@ void main(void)
     InitializeDeviceHive(&UartSendBytes, &UartRecvBytes);
 
     SendRegMessage();
+
+    g_prevBtnState = !(P1IN&BTN_PIN);
+    SendEquipmentNotification(BTN_EQP_CODE, g_prevBtnState);
+    SendEquipmentNotification(LED0_EQP_CODE, (P1OUT&RLED_PIN)!=0);
+    SendEquipmentNotification(LED1_EQP_CODE, (P1OUT&GLED_PIN)!=0);
+
     while (TRUE)
     {
         BYTE btn = !(P1IN&BTN_PIN);
@@ -237,12 +191,13 @@ void main(void)
                             SendEquipmentNotification(BTN_EQP_CODE, g_prevBtnState);
                         }
                     } break;
+
                     case G2D_LED:
                         ProcessCommand(&msgh);
                         break;
+
                     default:
                         SkipMessage(msgh.Length);
-                        //SendNotificationMessage(0, ERR_UNKNOWN_CMD, ERR_FAIL);
                         break;
                 }
             }
